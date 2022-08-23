6a390663df675e43af002c81ed654139b5314021
Veronika Ischenko <niki6800@gmail.com>
2022-08-23 15:14:30 +0300

    animation and page caching

--- app/src/main/java/com/example/hdeck/data_source/CardsPagingSource.kt
+++ app/src/main/java/com/example/hdeck/data_source/CardsPagingSource.kt
@@ -16,7 +16,6 @@ import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 
 
-
 class CardsPagingSource @AssistedInject constructor(
     private val authService: AuthService,
     private val localeService: LocaleService,
@@ -35,37 +34,38 @@ class CardsPagingSource @AssistedInject constructor(
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, CardApi> {
         val pageNumber = params.key ?: 1
-        val response = getCards(pageNumber, params.loadSize)
-        val data = response.cards
-        data.removeIf { it.copyOfCardId != 0 }
-
-        return LoadResult.Page(
-            data = data,
-            prevKey = if (pageNumber > 1) pageNumber - 1 else null,
-            nextKey = if (response.pageCount == pageNumber) null else pageNumber + 1
-        ).also { cardsService.setPage(pageNumber-1, it) }
+        val page = cardsService.getPage(slug, pageNumber)
+        return if (page != null) page
+        else {
+            val response = getCards(pageNumber, params.loadSize)
+            val data = response.cards
+            data.removeIf { card -> card.copyOfCardId != 0 }
+            LoadResult.Page(
+                data = data,
+                prevKey = if (pageNumber > 1) pageNumber - 1 else null,
+                nextKey = if (response.pageCount == pageNumber) null else pageNumber + 1
+            ).also { cardsService.setPage(slug, pageNumber, it) }
+        }
     }
 
     private suspend fun getCards(page: Int, pageSize: Int): Cards {
         val accessToken = authService.getToken()
-        val locale = localeService.getLanguage().also {
-            cardsService.setLanguage(it)
-        }
+        val locale = localeService.getApiLocale()
         return when (category) {
             Category.CardSet -> retrofit.getCardList(
-                locale.apiCode,
+                locale,
                 page,
                 pageSize,
                 accessToken, set = slug
             )
             Category.HeroClass -> retrofit.getCardList(
-                locale.apiCode,
+                locale,
                 page,
                 pageSize,
                 accessToken, heroClass = slug
             )
             Category.CardRarity -> retrofit.getCardList(
-                locale.apiCode,
+                locale,
                 page,
                 pageSize,
                 accessToken, rarity = slug
--- app/src/main/java/com/example/hdeck/service/CardsService.kt
+++ app/src/main/java/com/example/hdeck/service/CardsService.kt
@@ -6,11 +6,12 @@ import com.example.hdeck.data_source.RetrofitDataSource
 import com.example.hdeck.localization.LocaleService
 import com.example.hdeck.model.CardApi
 import com.example.hdeck.model.Language
+import com.example.hdeck.model.enums.Category
 import javax.inject.Inject
 
 interface CardsService {
-    suspend fun setLanguage(language: Language)
-    suspend fun setPage(index: Int, page: PagingSource.LoadResult.Page<Int, CardApi>)
+    suspend fun setPage(slug: String, pageNumber: Int, page: PagingSource.LoadResult.Page<Int, CardApi>)
+    suspend fun getPage(slug: String, pageNumber: Int): PagingSource.LoadResult.Page<Int, CardApi>?
     suspend fun getCard(slug: String): CardApi
 }
 
@@ -19,27 +20,49 @@ class CardsServiceImpl @Inject constructor(
     private val localeService: LocaleService,
     private val authService: AuthService,
 ) : CardsService {
-    private val pages: MutableList<PagingSource.LoadResult.Page<Int, CardApi>> = mutableListOf()
-    private var currentLocale: Language? = null
 
-    override suspend fun setLanguage(language: Language) {
+    private val pages: MutableMap<Int, PagingSource.LoadResult.Page<Int, CardApi>> = mutableMapOf()
+    private var currentLocale: Language? = null
+    private var currentSlug: String = ""
+
+    private fun setLanguage(language: Language) {
         if (currentLocale != language) {
             pages.clear()
             currentLocale = language
         }
     }
+    private fun setSlug(slug: String) {
+        if (currentSlug != slug) {
+            pages.clear()
+            currentSlug = slug
+        }
+    }
 
-    override suspend fun setPage(index: Int, page: PagingSource.LoadResult.Page<Int, CardApi>) {
-        while (pages.size < index + 1)
-            pages.add(PagingSource.LoadResult.Page(data = emptyList(), null, null))
-        pages[index] = page
+    override suspend fun setPage(
+        slug: String,
+        pageNumber: Int,
+        page: PagingSource.LoadResult.Page<Int, CardApi>
+    ) {
+        setSlug(slug)
+        if (pages.containsKey(pageNumber))
+            pages.replace(pageNumber, page)
+        else
+            pages[pageNumber] = page
+    }
+
+    override suspend fun getPage(slug: String, pageNumber: Int): PagingSource.LoadResult.Page<Int, CardApi>? {
+        setLanguage(localeService.getLanguage())
+        setSlug(slug)
+        return pages.getOrDefault(pageNumber, null)
     }
 
     override suspend fun getCard(slug: String): CardApi {
+        setLanguage(localeService.getLanguage())
         pages.forEach { page ->
-            return page.data.first { card ->
+            val card = page.value.data.firstOrNull { card ->
                 card.slug == slug
             }
+            if (card != null) return card
         }
         return dataSource.getCard(slug, localeService.getApiLocale(), authService.getToken())
     }
